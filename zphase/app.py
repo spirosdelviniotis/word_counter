12113b717bac8d278b949052c50b2ada44b3511d
Spiros Delviniotis <spirosdelviniotis@gmail.com>
2019-05-01 17:20:07 +0200

    global: refactor project structure
    * Ref: `.gitignore`.
    * Ref: configuration file for Heroku `Procfile` and `runtime.txt`.
    * Ref: simple `app` module.
    * Adds: configuration file for project `config.py`.
    
    Signed-off-by: Spiros Delviniotis <spirosdelviniotis@gmail.com>

--- zphase/app.py
+++ zphase/app.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import os
+
 from flask import Flask
 
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
 def hello():
     return 'Hello world!'
+
+
+@app.route('/<name>')
+def hello_name(name='casper'):
+    return "Hello {}!".format(name)
 
 
 if __name__ == '__main__':
