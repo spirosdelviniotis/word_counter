55e1b814eadfb8c498be557976da06e675a40e61
Spiros Delviniotis <spirosdelviniotis@gmail.com>
2019-05-08 06:47:14 +0200

    global: set up db
    * Adds: `models`, `manage` modules to support db.
    * Adds: `.env_pycharm` for IDE development.
    * Refactors: `README` introducing setup instructions.
    
    Signed-off-by: Spiros Delviniotis <spirosdelviniotis@gmail.com>

--- src/app.py
+++ src/app.py
@@ -3,10 +3,16 @@
 import os
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# from models import Result
+from models import *
 
 
 @app.route('/')
