380336a594159a7cda0c085bedcf7c2eeeca4e67
Spiros Delviniotis <spirosdelviniotis@gmail.com>
2019-05-12 16:02:15 +0200

    static: support D3
    * Adds: support of D3 plot.
    * Adds: spinner when processing results.
    * Refactors: `README`.
    
    Signed-off-by: Spiros Delviniotis <spirosdelviniotis@gmail.com>

--- README.rst
+++ README.rst
@@ -6,17 +6,27 @@ Word Counter
 Tutorial (https://realpython.com/flask-by-example-part-1-project-setup/)
 
 
-How to execute:
+Useful tips
+===========
+
+Creating a Heroku app (app-name=serene-beach-63709):
 
 .. code-block::
 
-    Creating a Heroku app (app-name=serene-beach-63709)
+    ~/repos/zphase -  (master) $ heroku login -i
+    ~/repos/zphase -  (master) $ heroku create <<app-name>>
+    ~/repos/zphase -  (master) $ heroku config:set APP_SETTINGS=config.DevelopmentConfig --remote heroku
+    ~/repos/zphase -  (master) $ heroku run python src/app.py --app serene-beach-63709
+    ~/repos/zphase -  (master) $ git push heroku master
 
-        ~/repos/zphase -  (master) $ heroku login -i
-        ~/repos/zphase -  (master) $ heroku create <<app-name>>
-        ~/repos/zphase -  (master) $ heroku config:set APP_SETTINGS=config.DevelopmentConfig --remote heroku
-        ~/repos/zphase -  (master) $ heroku run python src/app.py --app serene-beach-63709
-        ~/repos/zphase -  (master) $ git push heroku master
+
+Local simulation before pushing to Heroku:
+
+.. code-block::
+
+    ~/repos/zphase -  (master) $ heroku local
+
+Local db creation and migration of models:
 
 .. code-block::
 
@@ -24,26 +34,25 @@ How to execute:
     ~/repos/zphase/src -  (master) $python src/manage.py db migrate
     ~/repos/zphase/src -  (master) $python src/manage.py db upgrade
 
-.. code-block::
-
-    Configuring Postgres on Heroku:
-
-        ~/repos/zphase -  (master) $ heroku addons:create heroku-postgresql:hobby-dev --app serene-beach-63709
-        ~/repos/zphase -  (master) $ heroku config --app serene-beach-63709
-        ~/repos/zphase -  (master) $ heroku run src/python manage.py db upgrade --app serene-beach-63709
+Configuring Postgres on Heroku:
 
 .. code-block::
 
-    If you are not getting your jobs done, make sure you have 1) the worker listening 2) the redis server up 3) the manage.py running.
+    ~/repos/zphase -  (master) $ heroku addons:create heroku-postgresql:hobby-dev --app serene-beach-63709
+    ~/repos/zphase -  (master) $ heroku config --app serene-beach-63709
+    ~/repos/zphase -  (master) $ heroku run src/python manage.py db upgrade --app serene-beach-63709
 
-        Terminal 1: redis-server
-        Terminal 2: python manage.py runserver
-        Terminal 3: python worker.py
+If you are not getting your jobs done, make sure you have (1) the worker listening (2) the redis server up (3) the manage.py running.
 
 .. code-block::
 
-    Configuring Redis on Heroku:
+    Terminal 1: redis-server
+    Terminal 2: python manage.py runserver
+    Terminal 3: python worker.py
 
-        ~/repos/zphase -  (master) $ heroku addons:create redistogo:nano --app serene-beach-63709
-        ~/repos/zphase -  (master) $ heroku config --app wordcount-stage | grep REDISTOGO_URL
-        ~/repos/zphase -  (master) $ heroku local
+Configuring Redis on Heroku:
+
+.. code-block::
+
+    ~/repos/zphase -  (master) $ heroku addons:create redistogo:nano --app serene-beach-63709
+    ~/repos/zphase -  (master) $ heroku config --app wordcount-stage | grep REDISTOGO_URL
--- static/main.js
+++ static/main.js
@@ -6,31 +6,10 @@
 
         .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
             function($scope, $log, $http, $timeout) {
-                function getWordCount(jobID) {
-                    var timeout = "";
-
-                    var poller = function() {
-                    // fire another request
-                    $http.get('/results/'+jobID).
-                        success(function(data, status, headers, config) {
-                            if(status === 202) {
-                                $log.log(data, status);
-                            } else if (status === 200){
-                                $log.log(data);
-                                $scope.wordcounts = data;
-                                $timeout.cancel(timeout);
-                                return false;
-                            }
-                            // continue to call the poller() function every 2 seconds
-                            // until the timeout is cancelled
-                            timeout = $timeout(poller, 2000);
-                        });
-                    };
-                    poller();
-                }
+                $scope.submitButtonText = 'Submit';
+                $scope.loading = false;
 
                 $scope.getResults = function() {
-
                     $log.log("test");
 
                     // get the URL from the input
@@ -41,13 +20,74 @@
                         success(function(results) {
                             $log.log(results);
                             getWordCount(results);
+                            $scope.wordcounts = null;
+                            $scope.loading = true;
+                            $scope.submitButtonText = 'Loading...';
+                            $scope.urlerror = false;
                         }).error(function(error) {
                             $log.log(error);
                         });
 
                 };
-            }
-        ]);
 
+                function getWordCount(jobID) {
+                    var timeout = "";
+                    var poller = function() {
+                        // fire another request
+                        $http.get('/results/'+jobID).
+                        success(function(data, status, headers, config) {
+                            if(status === 202) {
+                                $log.log(data, status);
+                            } else if (status === 200){
+                                $log.log(data);
+                                $scope.loading = false;
+                                $scope.submitButtonText = "Submit";
+                                $scope.wordcounts = data;
+                                $timeout.cancel(timeout);
+                                return false;
+                            }
+                            // continue to call the poller() function every 2 seconds
+                            // until the timeout is cancelled
+                            timeout = $timeout(poller, 2000);
+                            }).error(function(error) {
+                                $log.log(error);
+                                $scope.loading = false;
+                                $scope.submitButtonText = "Submit";
+                                $scope.urlerror = true;
+                        });
+                    };
+
+                    poller();
+                }
+
+                }])
+                .directive('wordCountChart', ['$parse', function ($parse) {
+                    return {
+                        restrict: 'E',
+                        replace: true,
+                        template: '<div id="chart"></div>',
+                        link: function (scope) {
+                            scope.$watch('wordcounts', function() {
+                                d3.select('#chart').selectAll('*').remove();
+                                var data = scope.wordcounts;
+                                for (var word in data) {
+                                    d3.select('#chart')
+                                      .append('div')
+                                      .selectAll('div')
+                                      .data(word[0])
+                                      .enter()
+                                      .append('div')
+                                      .style('width', function() {
+                                        return (data[word][1] * 20) + 'px';
+                                      })
+                                      .text(function(d){
+                                        return data[word][0];
+                                      });
+                                }
+                            }, true);
+                        }
+                    };
+                }
+            ]);
     }()
 );
